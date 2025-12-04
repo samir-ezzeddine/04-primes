87d5eba1c03d7abc90e6d48310ff57186c451d83
samir-ezzeddine <samir.ezzeddine@edu.esiee.fr>
2025-12-04 09:02:05 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+"""Module pour tester si un entier est un nombre premier ou non."""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """Retourne True si p est un nombre premier, sinon False."""
+    if p < 2:
+        return False
 
-    # votre code ici
+    if p ==2:
+        return True
 
-    pass
+    if p % 2 == 0:
+        return False
 
+    for i in range (2, int(sqrt(p)) + 1 ):
+        if p % i == 0:
+            return False
+
+    return True
 #### Fonction principale
 
 
 def main():
 
+    """Affiche tous les nombres premiers strictement inférieurs à 100."""
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
